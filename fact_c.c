12bd0506f95466811b53bf77cfe4ac5d287efb7e
Poojakhane-25 <58602112+Poojakhane-25@users.noreply.github.com>
2019-12-10 15:07:49 +0530

    Update fact_c.c

--- fact_c.c
+++ fact_c.c
@@ -1,10 +1,10 @@
 #include <stdio.h> 
 int main()    
 {    
- int i,fact=1,number;    
+ int fact=1,number;    
  printf("Enter a number: ");    
   scanf("%d",&number);    
-    for(i=1;i<=number;i++){    
+    for( int i=1;i<=number;i++){    
       fact=fact*i;    
   }    
   printf("Factorial of %d is: %d",number,fact);    
